f45b2aa8966135483f478cac4cfc9702a1e09c98
Jonathan Kula <jonathan@jdkula.dev>
2023-02-09 02:18:32 -0800

    Commit build

--- websrc/svelte.config.js
+++ websrc/svelte.config.js
@@ -1,5 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { execSync } from 'child_process';
+
+// This snapshots all the website source files with sha256sum, then calculates the sha256 hash of that,
+//  to create a stable version name for the website.
+const snapshot = execSync(`bash -c 'find src static svelte.config.js package.json package-lock.json tsconfig.json vite.config.ts -type f -exec sha256sum "{}" + | grep -o "^[a-z0-9]* " | sort | sha256sum | grep -o "^[a-z0-9]* "' `).toString().trim();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,6 +19,9 @@ const config = {
 		},
 		alias: {
 			$: './src'
+		},
+		version: {
+			name: snapshot
 		}
 	}
 };
