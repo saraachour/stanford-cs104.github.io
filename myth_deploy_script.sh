20936655ce79fcae8836834a9fd5465b724d4c73
Jonathan Kula <jonathan@jdkula.dev>
2023-02-12 01:35:46 -0800

    2nd attempt using github API calls

--- myth_deploy_script.sh
+++ myth_deploy_script.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
+JQ="/afs/.ir/class/cs45/jq"
 AUTHCODE_LOCATION="/afs/.ir/class/cs45/deploy/deploy_code"
-DEPLOY_TMP="/afs/.ir/class/cs45/deploy/deploy.tar.gz"
-UNTAR_TMP="/afs/.ir/class/cs45/deploy/untar"
+DEPLOY_ROOT="/afs/.ir/class/cs45/deploy"
 OUTPUT="/afs/.ir/class/cs45/WWW"
 
 printf "Content-Type: text/plain\r\n";
 printf "\r\n";
-
-printf "$(pwd)\r\n";
-printf "$USER\r\n";
 
 if [[ -z "$HTTP_X_AUTH_CODE" ]]; then
   printf "Auth code must be provided\r\n";
@@ -22,24 +19,24 @@ if [[ "$HTTP_X_AUTH_CODE" != "$(cat $AUTHCODE_LOCATION)" ]]; then
 fi
 
 if [[ -z "$CONTENT_LENGTH" ]]; then
-  printf "Must provide endpoint\r\n";
+  printf "Must provide auth token\r\n";
   exit 1;
 fi
 
+AUTH_TOKEN="$(cat)"
 
-ARTIFACT_ID="$(cat)"
+DOWNLOAD_URL="$(curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $AUTH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/stanford-cs45/stanford-cs45.github.io/actions/artifacts | $JQ -r '.artifacts | sort_by(.created_at) | .[-1] | .archive_download_url')"
+curl -Lo "$DEPLOY_ROOT/deploy.zip" -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $AUTH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" $DOWNLOAD_URL
+unzip "$DEPLOY_ROOT/deploy.zip"
 
-curl \
-  -Lo "$DEPLOY_TMP" \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/stanford-cs45/stanford-cs45.github.io/actions/artifacts/$ARTIFACT_ID" && \
-  tar -zxvf "$DEPLOY_TMP" "$UNTAR_TMP" && \
-  rsync -a --delete "$UNTAR_TMP/" "$OUTPUT/" && \
-  rm -rf "$DEPLOY_TMP" "$UNTAR_TMP";
+
+mkdir -p "$DEPLOY_ROOT/deploy_untar"
+tar -zxvf "$DEPLOY_ROOT/deploy.tar.gz" -C "$DEPLOY_ROOT/deploy_untar" && \
+  rsync -a --delete "$DEPLOY_ROOT/deploy_untar/" "$OUTPUT/";
+
+rm -rf "$DEPLOY_ROOT/deploy_untar" "$DEPLOY_ROOT/deploy.tar.gz" "$DEPLOY_ROOT/deploy.zip";
 
 if [[ $? -ne 0 ]]; then
-  rm -rf "$DEPLOY_TMP" "$UNTAR_TMP";
   printf "Failed\r\n";
   exit 1;
 fi
