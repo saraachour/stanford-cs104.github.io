7a6f0e7d3366a39657d39f9ce483388ab083736f
Jonathan Kula <jonathan@jdkula.dev>
2023-02-23 23:19:12 -0800

    Remove Code since we can use markdown now + update software install page

--- src/lib/Callout.svelte
+++ src/lib/Callout.svelte
@@ -1,15 +1,25 @@
 <!-- Creates a callout block, with an optional yellow warning strip -->
-
 <script lang="ts">
-	export let warning: boolean = false;
+  export let warning: boolean = false;
+  export let info: boolean = false;
+  export let critical: boolean = false;
 </script>
 
-<div class="alert" class:alert-warning={warning}>
-	<slot />
+<div
+  class="alert"
+  class:alert-warning={warning}
+  class:alert-info={info}
+  class:alert-critical={critical}
+>
+  <slot />
 </div>
 
 <style>
   .alert {
-    overflow-x: scroll
+    overflow-x: scroll;
+  }
+
+  .alert > :global(p:first-of-type), .alert > :global(p:last-of-type) {
+    margin: 0;
   }
 </style>
