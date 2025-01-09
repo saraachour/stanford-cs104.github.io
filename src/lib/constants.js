e8e2fceb0c120542a0256071fa86bee56aceeb96
devStorm <59678453+developStorm@users.noreply.github.com>
2025-01-08 17:02:55 -0800

    chore: update course infra links

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,8 @@
 import { base } from '$app/paths';
 
-const edstem = 'https://edstem.org/us/courses/51628/discussion/';
-const gradescope = 'https://www.gradescope.com/courses/696269';
-const canvas = 'https://canvas.stanford.edu/courses/185708';
-const drive = 'https://drive.google.com/drive/folders/1-FdKO9OGhpetQudtemBIDCmSTSnDCCPw';
+const edstem = 'https://edstem.org/us/courses/71475/discussion';
+const gradescope = 'https://www.gradescope.com/courses/940340';
+const canvas = 'https://canvas.stanford.edu/courses/202816';
+const drive = 'https://drive.google.com/drive/folders/1r3fc0DfUnY0sVgx-AfKv55uTqfCH71zP';
 
 export { base, edstem, gradescope, canvas, drive };
