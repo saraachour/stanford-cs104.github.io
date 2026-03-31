dc74922a4afe666e6b2dc3efbe2903dcf0204b1d
Kelvin-Waititu <kelvinwaititu3@gmail.com>
2026-03-30 17:32:15 -0700

    Updated drive link

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,6 +3,6 @@ import { base } from '$app/paths';
 const edstem = 'https://edstem.org/us/courses/71475/discussion';
 const gradescope = 'https://www.gradescope.com/courses/940340';
 const canvas = 'https://canvas.stanford.edu/courses/202816';
-const drive = 'https://drive.google.com/drive/folders/1r3fc0DfUnY0sVgx-AfKv55uTqfCH71zP';
+const drive = 'https://drive.google.com/drive/folders/19zGCd8b29UHRrXloM4psml5DY4DxekL5?usp=drive_link';
 
 export { base, edstem, gradescope, canvas, drive };
